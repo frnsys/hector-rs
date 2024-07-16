32ca8bad7f347fa9ce890040f3d709f822b0881d
Francis Tseng <f+accounts@frnsys.com>
2024-07-16 18:46:54 +0200

    renamed to hector

--- examples/example.rs
+++ examples/example.rs
@@ -1,11 +1,15 @@
-use hector_rs::{run_hector, emissions::{START_YEAR, get_emissions}};
+use hector::{
+    emissions::{get_emissions, START_YEAR},
+    run_hector,
+};
 
 fn main() {
     let end_year = 2100;
     let emissions = get_emissions(2050);
-    assert_eq!(emissions["simpleNbox"]["ffi_emissions"].len(), 2050-START_YEAR);
-    let tgav = unsafe {
-        run_hector(end_year, &emissions)
-    };
+    assert_eq!(
+        emissions["simpleNbox"]["ffi_emissions"].len(),
+        2050 - START_YEAR
+    );
+    let tgav = unsafe { run_hector(end_year, &emissions) };
     println!("tgav: {:?}", tgav);
 }
